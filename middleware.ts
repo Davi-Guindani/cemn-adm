08d42421abb096159221bc20c405e2973afcd5f7
Davi-Guindani <davigpvieira@gmail.com>
2024-12-20 14:36:11 -0300

    add basic middleware setup

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,16 @@
-export function middleware() {}
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const loggedIn: boolean = false;
+
+export function middleware(request: NextRequest) {
+  if (loggedIn) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/protected_route/:path*"],
+};
