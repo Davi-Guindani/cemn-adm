12d1a0e2f6ef88401f2a5fd822171d44dab08ae1
Davi-Guindani <davigpvieira@gmail.com>
2024-12-23 10:38:37 -0300

    changing the logout redirect to /

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -14,7 +14,7 @@ export default async function AuthButton() {
 
     const supabase = await createClient();
     await supabase.auth.signOut();
-    return redirect("/login");
+    return redirect("/");
   };
 
   return user ? (
