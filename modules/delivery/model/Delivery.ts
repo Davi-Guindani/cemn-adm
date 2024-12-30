8553fc5db8be1781e319b0e3e26a3fa1dd119788
Davi-Guindani <davigpvieira@gmail.com>
2024-12-29 21:50:56 -0300

    adjust email info on tables

--- modules/delivery/model/Delivery.ts
+++ modules/delivery/model/Delivery.ts
@@ -1,38 +1,52 @@
 import { Record } from "modules/record/model/Record";
 
 export class Delivery extends Record {
-  private _residentId: string;
-  private _company: string;
-  private _userId: string;
-  private _receivedAt: string;
+  private readonly _userId: string;
+  private readonly _houseId: string;
+  private readonly _streetId: string;
+  private readonly _residentId: string;
+  private readonly _receivedAt: Date;
+  private readonly _company: string;
 
   constructor(
     id: string,
-    residentId: string,
-    company: string,
     userId: string,
-    receivedAt: string,
+    streetId: string,
+    houseId: string,
+    residentId: string,
+    receivedAt: Date,
+    company: string,
   ) {
     super(id);
-    this._residentId = residentId;
-    this._company = company;
     this._userId = userId;
+    this._streetId = streetId;
+    this._houseId = houseId;
+    this._residentId = residentId;
     this._receivedAt = receivedAt;
-  }
-
-  public get residentId(): string {
-    return this._residentId;
-  }
-
-  public get company(): string {
-    return this._company;
+    this._company = company;
   }
 
   public get userId(): string {
     return this._userId;
   }
 
-  public get receivedAt(): string {
+  public get streetId(): string {
+    return this._streetId;
+  }
+
+  public get houseId(): string {
+    return this._houseId;
+  }
+
+  public get residentId(): string {
+    return this._residentId;
+  }
+
+  public get receivedAt(): Date {
     return this._receivedAt;
+  }
+
+  public get company(): string {
+    return this._company;
   }
 }
--- modules/delivery/service/delivery.ts
+++ modules/delivery/service/delivery.ts
@@ -1,10 +1,13 @@
 import { createClient } from "utils/supabase/server";
 import { Delivery } from "modules/delivery/model/Delivery";
 
-export async function getDeliveries() {
+export async function getAllDeliveries() {
   const supabase = await createClient();
   const { data, error } = await supabase.from("deliveries").select("*");
 
-  if (error) throw new Error(error.message);
-  return data;
+  if (!error) {
+    return data;
+  } else {
+    throw new Error(error.message);
+  }
 }
